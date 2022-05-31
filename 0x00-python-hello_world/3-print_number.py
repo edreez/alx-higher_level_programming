e736dc15007bf32cf3930eed31159edd1f198946
edreez <olawunmiolatunji77@gmail.com>
2022-05-31 00:59:44 +0000

    print integers

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 number = 98
-print("{:d} Battery street".format(number))
+#print("{:d} Battery street".format(number))
+python3 -c "import py_compile; py_compile.compile('$PYFILE', '$PYFILE' + 'c')"
+
