ab4cdbe22c5fff72a7a152654e57565c5581158d
edreez <olawunmiolatunji77@gmail.com>
2022-06-01 21:44:32 +0000

    floating number

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 98
-#print("{number:d} Battery street")
+print("{number:d} Battery street")
 python3 -c "import py_compile; py_compile.compile('$PYFILE', '$PYFILE' + 'c')"
