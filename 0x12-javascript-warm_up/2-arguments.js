e8dda03773c4472ef8b17f43c6cfaf5715655f9c
edreez <olawunmiolatunji77@gmail.com>
2022-09-06 04:05:32 +0000

    arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 'use strict';
 let w = process.argv.length;
 if (w == 2) {
-	  console.log('No argument');
+  console.log('No argument');
 } else if (w == 3) {
-	  console.log('Argument found');
+  console.log('Argument found');
 } else {
-	  console.log('Arguments found');
+  console.log('Arguments found');
 }
