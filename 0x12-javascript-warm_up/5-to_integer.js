bad5c3fe73f9762ba7d5a6fb060723a513cbd63d
edreez <olawunmiolatunji77@gmail.com>
2022-09-06 04:36:52 +0000

    to integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-'use strict';
-let arg = process.argv[2];
-if (isNaN(arg)) {
-	  console.log('Not a number');
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Not a number');
 } else {
-	  console.log('My number: ' + arg);
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
